b5473ae8276ab1334ad88c35693e393be5e3fe65
SanyaKush86 <oleksandr.kushnirr@gmail.com>
2023-05-25 20:07:02 +0300

    ddd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,172 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class Main extends TelegramLongPollingBot {
+    private Map<Long, Integer> levels = new HashMap<>();
+    public static void main(String[] args) throws TelegramApiException {
+    TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+    api.registerBot(new Main());
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "BanderogusGoitBotKushnir_bot";
+    }
+    @Override
+    public String getBotToken() {
+        return "6035041708:AAFemK5OCTOE326RoZIPM61tZUylMq4bzKI";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatId = getChatId(update);
+
+      if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
+          sendImage("level-1", chatId);
+          SendMessage message = createMassage("Ґа-ґа-ґа!\n" +
+                  "Вітаємо у боті біолабораторії «Батько наш Бандера».\n" +
+                  "\n" +
+                  "Ти отримуєш гусака №71\n" +
+                  "\n" +
+                  "Цей бот ми створили для того, щоб твій гусак прокачався з рівня звичайної свійської худоби до рівня біологічної зброї, здатної нищити ворога. \n" +
+                  "\n" +
+                  "Щоб звичайний гусак перетворився на бандерогусака, тобі необхідно:\n" +
+                  "✔виконувати завдання\n" +
+                  "✔переходити на наступні рівні\n" +
+                  "✔заробити достатню кількість монет, щоб придбати Джавеліну і зробити свєрхтра-та-та.\n" +
+                  "\n" +
+                  "*Гусак звичайний.* Стартовий рівень.\n" +
+                  "Бонус: 5 монет.\n" +
+                  "Обери завдання, щоб перейти на наступний рівень\n");
+          message.setChatId(chatId);
+          attachButtons(message, Map.of(
+                  "Сплести маскувальну сітку (+15 монет)", "level_1_task",
+                  "Зібрати кошти патріотичними піснями (+15 монет)", "level_1_task",
+                  "Вступити в Міністерство Мемів України (+15 монет)", "level_1_task"
+
+          ));
+          sendApiMethodAsync(message);
+      }
+            if (update.hasCallbackQuery()) {
+                if (update.getCallbackQuery().getData().equals("level_1_task") && getLevel(chatId) == 1) {
+                    setLevel(chatId, 2);
+//                    send image
+                    sendImage("level-2", chatId);
+                   SendMessage message = createMassage("*Вітаємо на другому рівні! Твій гусак - біогусак.*\n" +
+                            "Баланс: 20 монет. \n" +
+                            "Обери завдання, щоб перейти на наступний рівень");
+                    message.setChatId(chatId);
+                    attachButtons(message, Map.of(
+                            "Зібрати комарів для нової біологічної зброї (+15 монет)", "level_2_task",
+                            "Пройти курс молодого бійця (+15 монет)", "level_2_task",
+                            "Задонатити на ЗСУ (+15 монет)", "level_2_task"
+                    ));
+                    sendApiMethodAsync(message);
+                }
+                if (update.getCallbackQuery().getData().equals("level_2_task") && getLevel(chatId) == 2) {
+                    setLevel(chatId, 3);
+//                    send image
+                    sendImage("level-3", chatId);
+                    SendMessage message = createMassage("*Вітаємо на третьому рівні! Твій гусак - бандеростажер.*\n" +
+                            "Баланс: 35 монет. \n" +
+                            "Обери завдання, щоб перейти на наступний рівень");
+                    message.setChatId(chatId);
+                    attachButtons(message, Map.of(
+                            "Злітати на тестовий рейд по чотирьох позиціях (+15 монет)", "level_2_task",
+                            "Відвезти гуманітарку на передок (+15 монет)", "level_2_task",
+                            "Знайти зрадника та здати в СБУ (+15 монет)", "level_2_task"
+                    ));
+                    sendApiMethodAsync(message);
+                }
+
+            }
+//          SendMessage message = createMassage("Привіт");
+//          message.setChatId(chatId);
+//          attachButtons(message, Map.of(
+//                  "Слава Україні!", "glory_for_ukraine"
+//          ));
+//          sendApiMethodAsync(message);
+//      }
+//        if (update.hasCallbackQuery()) {
+//            if (update.getCallbackQuery().getData().equals("glory_for_ukraine")) {
+//                SendMessage message = createMassage("Героям слава!");
+//                message.setChatId(chatId);
+//                attachButtons(message, Map.of(
+//                        "Слава нації!", "glory_to_the_nation"
+//                ));
+//                sendApiMethodAsync(message);
+//            }
+//            if (update.getCallbackQuery().getData().equals("glory_to_the_nation")) {
+//                SendMessage message = createMassage("Смерть ворогам!");
+//                message.setChatId(chatId);
+//                sendApiMethodAsync(message);
+//            }
+//        }
+    }
+    public Long getChatId (Update update) {
+        if (update.hasMessage()) {
+            return update.getMessage().getFrom().getId();
         }
+        if (update.hasCallbackQuery()) {
+            return update.getCallbackQuery().getFrom().getId();
+        }
+        return null;
+    }
+    public SendMessage createMassage (String text) {
+        SendMessage massage = new SendMessage();
+        massage.setText(new String(text.getBytes(), StandardCharsets.UTF_8));
+        massage.setParseMode("markdown");
+        return massage;
+
+    }
+    public void attachButtons(SendMessage message, Map<String, String> buttons) {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        for (String buttonName : buttons.keySet()) {
+            String buttonValue = buttons.get(buttonName);
+
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(new String(buttonName.getBytes(), StandardCharsets.UTF_8));
+            button.setCallbackData(buttonValue);
+            keyboard.add(Arrays.asList(button));
+        }
+        markup.setKeyboard(keyboard);
+        message.setReplyMarkup(markup);
+    }
+    public void sendImage(String name, Long chatId) {
+        SendAnimation animation = new SendAnimation();
+
+        InputFile inputFile = new InputFile();
+        inputFile.setMedia(new File("images/" + name + ".gif"));
+
+        animation.setAnimation(inputFile);
+        animation.setChatId(chatId);
+        executeAsync(animation);
+
+    }
+    public int getLevel(Long chatId) {
+        return levels.getOrDefault(chatId, 1);
+    }
+    public void setLevel(Long chatId, int level) {
+        levels.put(chatId, level);
     }
 }
