e5a77d6d19a3891f134c03a203bfd377abf123bb
SanyaKush86 <oleksandr.kushnirr@gmail.com>
2023-05-26 15:01:05 +0300

    finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -39,7 +39,9 @@ public class Main extends TelegramLongPollingBot {
         Long chatId = getChatId(update);
 
       if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
+          setLevel(chatId, 1);
           sendImage("level-1", chatId);
+
           SendMessage message = createMassage("Ґа-ґа-ґа!\n" +
                   "Вітаємо у боті біолабораторії «Батько наш Бандера».\n" +
                   "\n" +
@@ -56,10 +58,21 @@ public class Main extends TelegramLongPollingBot {
                   "Бонус: 5 монет.\n" +
                   "Обери завдання, щоб перейти на наступний рівень\n");
           message.setChatId(chatId);
+
+          List <String> buttons = Arrays.asList(
+                  "Сплести маскувальну сітку (+15 монет)",
+                  "Зібрати кошти патріотичними піснями (+15 монет)",
+                  "Вступити в Міністерство Мемів України (+15 монет)",
+                  "Запустити волонтерську акцію (+15 монет)",
+                  "Вступити до лав тероборони (+15 монет)"
+
+          );
+          buttons = getRandom3(buttons);
+
           attachButtons(message, Map.of(
-                  "Сплести маскувальну сітку (+15 монет)", "level_1_task",
-                  "Зібрати кошти патріотичними піснями (+15 монет)", "level_1_task",
-                  "Вступити в Міністерство Мемів України (+15 монет)", "level_1_task"
+                  buttons.get(0), "level_1_task",
+                  buttons.get(1), "level_1_task",
+                  buttons.get(2), "level_1_task"
 
           ));
           sendApiMethodAsync(message);
@@ -73,11 +86,24 @@ public class Main extends TelegramLongPollingBot {
                             "Баланс: 20 монет. \n" +
                             "Обери завдання, щоб перейти на наступний рівень");
                     message.setChatId(chatId);
+
+                    List <String> buttons = Arrays.asList(
+                            "Зібрати комарів для нової біологічної зброї (+15 монет)",
+                            "Пройти курс молодого бійця (+15 монет)",
+                            "Задонатити на ЗСУ (+15 монет)",
+                            "Збити дрона банкою огірків (+15 монет)",
+                            "Зробити запаси коктейлів Молотова (+15 монет)"
+
+                    );
+                    buttons = getRandom3(buttons);
+
                     attachButtons(message, Map.of(
-                            "Зібрати комарів для нової біологічної зброї (+15 монет)", "level_2_task",
-                            "Пройти курс молодого бійця (+15 монет)", "level_2_task",
-                            "Задонатити на ЗСУ (+15 монет)", "level_2_task"
+                            buttons.get(0), "level_2_task",
+                            buttons.get(1), "level_2_task",
+                            buttons.get(2), "level_2_task"
+
                     ));
+
                     sendApiMethodAsync(message);
                 }
                 if (update.getCallbackQuery().getData().equals("level_2_task") && getLevel(chatId) == 2) {
@@ -88,11 +114,46 @@ public class Main extends TelegramLongPollingBot {
                             "Баланс: 35 монет. \n" +
                             "Обери завдання, щоб перейти на наступний рівень");
                     message.setChatId(chatId);
+
+                    List <String> buttons = Arrays.asList(
+                            "Злітати на тестовий рейд по чотирьох позиціях (+15 монет)",
+                            "Відвезти гуманітарку на передок (+15 монет)",
+                            "Знайти зрадника та здати в СБУ (+15 монет)",
+                            "Навести арту на орків (+15 монет)",
+                            "Притягнути танк трактором (+15 монет)"
+
+                    );
+                    buttons = getRandom3(buttons);
+
                     attachButtons(message, Map.of(
-                            "Злітати на тестовий рейд по чотирьох позиціях (+15 монет)", "level_2_task",
-                            "Відвезти гуманітарку на передок (+15 монет)", "level_2_task",
-                            "Знайти зрадника та здати в СБУ (+15 монет)", "level_2_task"
+                            buttons.get(0), "level_3_task",
+                            buttons.get(1), "level_3_task",
+                            buttons.get(2), "level_3_task"
+
                     ));
+
+                    sendApiMethodAsync(message);
+                }
+                if (update.getCallbackQuery().getData().equals("level_3_task") && getLevel(chatId) == 3) {
+                    setLevel(chatId, 4);
+//                    send image
+                    sendImage("level-4", chatId);
+                    SendMessage message = createMassage("*Вітаємо на останньому рівні! Твій гусак - готова біологічна зброя - бандерогусак.*\n" +
+                            "Баланс: 50 монет. \n" +
+                            "Тепер ти можеш придбати Джавелін і глушити чмонь");
+                    message.setChatId(chatId);
+                    attachButtons(message, Map.of(
+                        "Купити Джавелін (50 монет)", "buy_javeline"
+                ));
+
+                    sendApiMethodAsync(message);
+                }
+                if (update.getCallbackQuery().getData().equals("buy_javeline") && getLevel(chatId) == 4) {
+                    setLevel(chatId, 5);
+//                    send image
+                    sendImage("final", chatId);
+                    SendMessage message = createMassage("*Джавелін твій. Повний вперед!*");
+                    message.setChatId(chatId);
                     sendApiMethodAsync(message);
                 }
 
@@ -168,5 +229,11 @@ public class Main extends TelegramLongPollingBot {
     }
     public void setLevel(Long chatId, int level) {
         levels.put(chatId, level);
+    }
+    public List<String> getRandom3(List<String> variants) {
+        ArrayList<String> copy = new ArrayList<>(variants);
+        Collections.shuffle(copy);
+        return copy.subList(0,3);
+
     }
 }
